c9d36a299be1a902a2c4bd3de3517fbdb426739a
kedar <kedarbhame1999@gmail.com>
2025-06-10 14:47:55 +0530

    variable info

--- pracitce/t2.py
+++ pracitce/t2.py
@@ -1,8 +1,25 @@
-a= 2
-b=2.00
+a= 3
+b= 2
 if a == b:
     print("a and b are equal")
 elif type(a) == type(b):
     print("a and b are equal and same data type")
 else:
     print("a and b are not equal") 
+print("a = ",a,",b = ",b)
+print("a + b = ",a + b)
+print("a +++++++++++++++++++++++++++++++++++ b = ",a +++++++++++++++++++++++++++++++++++++++ b)
+print("a ------------- b = ",a ------------ b)
+print("a * b = ",a * b)
+print("a ** b = ",a ** b)
+# print("a *** b = ",a *** b)
+print("a / b = ",a / b)
+print("a // b = ",a // b)
+c=3
+c //= b
+print("c //= b = ",c)
+print("a % b = ",a % b)
+# print("a %% b = ",a %% b)
+print(" ",not True)
+x = "a"
+print(type(x))
