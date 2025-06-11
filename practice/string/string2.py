8f28dfb600ddeb0ced6dc3e4daedd6448842dbf5
KedarBhame01 <kedarbhame1999@gmail.com>
2025-06-11 21:06:00 +0530

    some string functions

---
+++ practice/string/string2.py
@@ -0,0 +1,11 @@
+str = "hi guys i am Kedar Bhame"
+print("str = ",str)
+print("type(str) = ",type(str))
+print("len(str) = ",len(str))
+print("str.endswith('me') = ",str.endswith("me"))
+print("str.capitalize() = ",str.capitalize())
+print("str.replace(guys,girls) = ",str.replace("guys","girls"))
+print("str.find('Kedar') = ",str.find('Kedar'))
+print("str.count('a') = ",str.count('a'))
+
+print("length of name = ", len(input("Enter first name : ")))
