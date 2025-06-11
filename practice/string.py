5f6e8293a308efc698747a566a0ee6c27d76f7e8
KedarBhame01 <kedarbhame1999@gmail.com>
2025-06-11 06:39:32 +0530

    string start from home

---
+++ practice/string.py
@@ -0,0 +1,16 @@
+# 4 ways to write string
+a = 'kedar'
+# b = ''kedar''
+c = '''kedar'''
+d = "kedar"
+# e = ""Kedar""
+f = """Kedar"""
+# g = """"Kedar""""
+
+print(a)
+# print(b)
+print(c)
+print(d)
+# print(e)
+print(f)
+# print(g)
