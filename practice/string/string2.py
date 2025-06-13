710883ee7838c43f76d4d7caae473a928c81c20e
KedarBhame01 <kedarbhame1999@gmail.com>
2025-06-13 07:48:45 +0530

    type() function is a metaclass

--- practice/string/string2.py
+++ practice/string/string2.py
@@ -8,4 +8,14 @@ print("str.replace(guys,girls) = ",str.replace("guys","girls"))
 print("str.find('Kedar') = ",str.find('Kedar'))
 print("str.count('a') = ",str.count('a'))
 
-print("length of name = ", len(input("Enter first name : ")))
+# print("length of name = ", len(input("Enter first name : ")))
+
+print("type(5) = ",type(5))
+print("type('Kedar'') = ",type("Kedar"))
+
+print("type([1,2,3]) = ",type([1,2,3]))
+print("type(int) = ",type(int))
+print("type(type) = ",type(type))
+
+print("type(True) = ",type(True))
+print("type(bool) = ",type(bool))
