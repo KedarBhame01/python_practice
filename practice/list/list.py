ad01ee0ffb9cc98edbf530dd3800df657bd1141b
kedar <kedarbhame1999@gmail.com>
2025-06-13 14:56:21 +0530

    list and tuple

---
+++ practice/list/list.py
@@ -0,0 +1,4 @@
+list=[2,1,3]
+print("list = ", list)
+print(" ",list.remove(1))
+print("list = ", list)
---
+++ practice/list/list2.py
@@ -0,0 +1,10 @@
+
+movie =[input("Enter first movie name: "),input("Enter second movie name: "),input("Enter third movie name: ")]
+print("movie = ", movie)
+# movie1 = input("Enter first movie name: ")
+# movie.append(movie1)
+# movie2 = input("Enter second movie name: ")
+# movie.append(movie2)
+# movie3 = input("Enter third movie name: ")
+# movie.append(movie3)
+# movie = [movie1, movie2, movie3]
