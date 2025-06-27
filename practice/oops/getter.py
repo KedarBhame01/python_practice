643b8f55a329f77318dda2866584c8f51e2cf002
kedar <kedarbhame1999@gmail.com>
2025-06-27 18:06:40 +0530

    Add Student class with getter and setter for name
    Introduces a Student class using Python property decorators to define getter and setter methods for the private __name attribute. Includes example usage demonstrating property access and modification.

---
+++ practice/oops/getter.py
@@ -0,0 +1,17 @@
+class Student:
+    def __init__(self,name):
+        self.__name = name
+    
+    @property
+    def name(self):         #getter
+        return self.__name
+    
+    @name.setter
+    def name(self,value):         #setter
+        self.__name = value
+        
+s1 = Student("Kedar")
+print(s1.name)
+s1.name = "Yk"
+print(s1.name)
+
